15be8ada3f1ffaa29c26ebd6b25901a50f750e1f
David Torres <davidthekoa@gmail.com>
2021-10-23 23:10:51 -0500

    Convert Double Value into Integer

---
+++ Cpp_Basics/5-Double_into_integer.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+/*Convert Double Value into Integer*/
+
+int main(){
+    double double_value = 70.986;
+    int int_value;
+    
+    int_value = (int) double_value;
+    cout << "int_value = " << int_value << endl;
+}
