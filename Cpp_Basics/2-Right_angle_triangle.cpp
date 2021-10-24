69c8a50ad5cc517ae570834027c5530202331258
David Torres <davidthekoa@gmail.com>
2021-10-23 21:25:04 -0500

    Displaying an up-right angle triangle using cout

---
+++ Cpp_Basics/2-Right_angle_triangle.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+/* Displaying an right-up angle triangle using cout*/
+
+int main (){
+    cout << "& & & & & &\n";
+    cout << "& & & & &\n";
+    cout << "& & & &\n";
+    cout << "& & &\n";
+    cout << "& &\n";
+    cout << "&\n";
+}
