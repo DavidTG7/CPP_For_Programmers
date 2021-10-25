84dd01886b278aa66c3e575d1c92c032e4e69853
David Torres <davidthekoa@gmail.com>
2021-10-25 17:24:54 -0500

    Find the person with the highest salary

---
+++ Cpp_Basics/9-Highest_salary.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+/* In this challenge, you are provided with the salaries of two people. 
+   Your task is to find the person with the highest salary. */
+
+int main(){
+    int salary1 = 56000, salary2 = 89000;
+
+    if (salary1 > salary2){
+        cout << "person1\n";
+    }
+    else
+        cout << "person2\n";
+}
