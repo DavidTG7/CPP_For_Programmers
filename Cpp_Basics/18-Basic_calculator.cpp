d9552082a9e2468b0f018764ba383c73cde652ca
David Torres <davidthekoa@gmail.com>
2021-11-03 22:38:33 -0500

    First commit for basic calculator

---
+++ Cpp_Basics/18-Basic_calculator.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+/* The aim of this challenge is to design a calculator that takes 
+   operands in its input and performs one of the following operations on them:
+
+Addition
+Subtraction
+Multiplication
+Division */
+
